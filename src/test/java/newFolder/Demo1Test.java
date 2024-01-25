3b9a5bb0195b45da69f2381675a91f9009bd7849
Niveditha1930 <Niveditha1930@gmail.com>
2024-01-25 15:38:56 +0530

    add project

---
+++ src/test/java/newFolder/Demo1Test.java
@@ -0,0 +1,13 @@
+package newFolder;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+	
+	@Test
+	public void runTest()
+	{
+		System.out.println("Test1");
+	}
+
+}
---
+++ src/test/java/newFolder/Demo2Test.java
@@ -0,0 +1,13 @@
+package newFolder;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+	
+	@Test
+	public void run2Test()
+	{
+		System.out.println("Test2");
+	}
+
+}
