ffc9ba3e0f6efff1c717f3811f6320ddcfcf1358
Niveditha1930 <Niveditha1930@gmail.com>
2024-01-29 13:46:10 +0530

    Commit

---
+++ src/test/java/newFolder/Demo9Test.java
@@ -0,0 +1,13 @@
+package newFolder;
+
+import org.testng.annotations.Test;
+
+public class Demo9Test {
+	
+	@Test
+	public void run9Test()
+	{
+		System.out.println("Added new 9th Program ");
+	}
+
+}
